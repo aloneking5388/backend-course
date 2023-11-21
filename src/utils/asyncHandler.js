5c705c16482c439faee89dc3e39fb5e7c899b3a1
alineking5388 <alineking5388@gmail.com>
2023-11-21 12:48:45 +0530

    commit delete

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,23 +5,4 @@ const asyncHandler = (requestHandler) => {
 }
 
 
-export {asyncHandler}
-
-// const asyncHandler = () => {}
-// const asyncHandler = (func) => () => {}
-// const asyncHandler = (func) => async () => {}
-
-
-
-
-
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// } 
+export {asyncHandler};
