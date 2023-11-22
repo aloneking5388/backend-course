b5052d03918f1fc24368a43db89f08699f091dab
alineking5388 <alineking5388@gmail.com>
2023-11-22 22:49:43 +0530

    bug fixex

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        lowecase: true,
+        lowercase: true,
         trim: true,
         index: true
     },
@@ -15,7 +15,7 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        lowecase: true,
+        lowercase: true,
         trim: true
     },
     fullName: {
@@ -49,7 +49,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
