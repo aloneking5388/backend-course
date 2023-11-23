a87f6b62464bb1e304a32f4becd8c8cbdc56fe6c
alineking5388 <alineking5388@gmail.com>
2023-11-23 22:34:14 +0530

    api testing done

--- src/controllers/user.cntroller.js
+++ src/controllers/user.cntroller.js
@@ -14,7 +14,7 @@ const registerUser = asyncHandler( async (req, res) => {
             throw new ApiError(400, "All fields are required")
         }
     // check if user already exists: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -23,7 +23,12 @@ const registerUser = asyncHandler( async (req, res) => {
         }
     //check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+
+        if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+            coverImageLocalPath = req.files.coverImage[0].path
+        }
          
         if (!avatarLocalPath) {
             throw new ApiError(400, "Avatar file is required")
